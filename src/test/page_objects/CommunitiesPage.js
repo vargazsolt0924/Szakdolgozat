18650d7da21b060dc398e629a864011ecbf036f2
vargazsolt0924 <vargazsolt1348@gmail.com>
2024-08-28 19:08:30 +0200

    EventsPage elkészítése

--- src/test/page_objects/CommunitiesPage.js
+++ src/test/page_objects/CommunitiesPage.js
@@ -5,16 +5,18 @@ class CommunitiesPage{
         this.emailField = page.locator("#username");
         this.continueButton = page.locator("#kc-login-next");
         this.passwordField = page.locator("#password");
-        this.verifyCheckbox = page.locator("");
+        this.verifyCheckbox = page.locator("#turnstileContainer input");
         this.signInButton = page.locator("#kc-login");
         this.searchInput = page.locator(".evnt-search-filter");
         this.joinButton = page.locator(".evnt-reg-wrapper");
         this.communityCards = page.locator(".evnt-card-wrapper");
         this.littleVideosButton= page.locator(".evnt-sub-header a[href='/communities/idea-pool/videos']");
-        this.playButton = page.locator("");
-        this.verifyCheckbox = page.locator("");
+        this.playButton = page.locator(".plyr__poster:nth-of-type(1)");
+        this.fullScreenButton = page.locator("");
         this.verifyCheckbox = page.locator("");
         this.verifyCheckbox = page.locator("");
         this.verifyCheckbox = page.locator("");
     }
 }
+
+module.exports = CommunitiesPage;
---
+++ src/test/page_objects/EventsPage.js
@@ -0,0 +1,18 @@
+class EventsPage{
+    constructor(page){
+        this.page =page;
+        this.loginButton = page.locator("evnt-header-button login");
+        this.emailField = page.locator("#username");
+        this.continueButton = page.locator("#kc-login-next");
+        this.passwordField = page.locator("#password");
+        this.verifyCheckbox = page.locator("#turnstileContainer input");
+        this.signInButton = page.locator("#kc-login");
+        this.moreFiltersOption = page.locator("evnt-toggle-filters-button btn");
+        this.languageFilter = page.locator("#filter_language");
+        this.languageFilterCheckbox = page.locator(".evnt-filter-item");
+        this.eventFormatOption = page.locator("#filter_event_participation_format");
+        this.offlineOnlyCheckbox = page.locator(".evnt-filter-item label[data-value='Offline only']");
+    }
+}
+
+module.exports = EventsPage;
